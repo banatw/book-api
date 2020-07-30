8c23f03d4892bae0d079f773c09d5e2cd04999b7
bana <syabana.wahyudi@gmail.com>
2020-07-30 15:14:06 +0700

    organize import

--- src/main/java/com/example/bookapi/entity/Author.java
+++ src/main/java/com/example/bookapi/entity/Author.java
@@ -3,12 +3,9 @@ package com.example.bookapi.entity;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
